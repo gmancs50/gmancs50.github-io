fe5fc51ce7acb31815fde5081f3ffe9e7e17e53b
gmancs50 <141275524+gmancs50@users.noreply.github.com>
2024-09-14 16:56:19 +0100

    new website for submission

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://foodish-api.com/images/burger/")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
-            image.width = 100;
-            image.height = 100;
+            image.width = 1000;
+            image.height = 1000;
     })
     }
 })
